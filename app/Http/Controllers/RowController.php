0cf7e57a043f8616c1ed27a48b300edcfb0536c2
jokermt2357 <jokermt2357@gmail.com>
2022-02-28 22:02:04 +0600

    Add Row controller  configure routing

---
+++ app/Http/Controllers/RowController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class RowController extends Controller
+{
+    /**
+     * Загрузка файла для парсинга.
+     *
+     * @param  request  $request
+     * @return \Response\
+     */
+    public function store()
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RowController;
+
+Route::resource('rows', RowController::class);
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
